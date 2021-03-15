b8ee16d91016aeb263fcb2cbe163b2b659348d14
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-03-15 06:47:22 +0200

    new mapping for processing the customer input

--- src/com/luv2code/springdemo/controller/CutomerController.java
+++ src/com/luv2code/springdemo/controller/CutomerController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.luv2code.springdemo.entity.Customer;
 import com.luv2code.springdemo.service.CustomerService;
@@ -28,6 +30,25 @@ public class CutomerController {
 		theModel.addAttribute("customers",theCustomers);
 		
 		return "list-customers";
+	}
+	
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel) {
+		
+		// create model attribute to bind form the data
+		Customer newCustomer = new Customer();
+		
+		theModel.addAttribute("customer",newCustomer);
+		
+		return "customer-form";
+	}
+	
+	@PostMapping("/saveCustomer")
+	public String saveCustomer(@ModelAttribute("customer")Customer theCustomer) {
+		
+		// save the customer using service
+		customerService.saveCustomer(theCustomer);
+		return "redirect:/customer/list";
 	}
 
 }
